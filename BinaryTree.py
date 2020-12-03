070093646c03aedd43f312dd16baac6f99444900
trichau123 <53028788+trichau123@users.noreply.github.com>
2020-12-03 00:51:13 -0500

    Add files via upload

---
+++ BinaryTree.py
@@ -0,0 +1,49 @@
+import random
+class Node:
+
+    def __init__(self, data):
+
+        self.left = None
+        self.right = None
+        self.data = data
+
+    def insert(self, data):
+# Compare the new value with the parent node
+        if self.data:
+            if data < self.data:
+                if self.left is None:
+                    self.left = Node(data)
+                else:
+                    self.left.insert(data)
+            elif data > self.data:
+                if self.right is None:
+                    self.right = Node(data)
+                else:
+                    self.right.insert(data)
+        else:
+            self.data = data
+
+# Print the tree
+    def PrintTree(self):
+        if self.left:
+            self.left.PrintTree()
+        print( self.data),
+        if self.right:
+            self.right.PrintTree()
+
+
+
+
+
+#     3
+# 0     4
+#   2      8
+a = random.sample(range(18), 12)
+print (a)
+tree = Node(a[0])
+for i in range(0,12):
+  tree.insert(a[i])
+
+
+
+tree.PrintTree()
